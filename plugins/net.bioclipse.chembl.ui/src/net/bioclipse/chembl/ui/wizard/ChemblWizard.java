d84d66133c7e4e291ee37985cb1afba8369eaf0a
annzi <annzi.andersson@gmail.com>
2010-06-29 15:39:53 +0200

    added WizardNewFileCreationPage

--- plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/ChemblWizard.java
+++ plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/ChemblWizard.java
@@ -22,108 +22,92 @@ import java.util.ArrayList;
 
 import net.bioclipse.chembl.Activator;
 import net.bioclipse.chembl.business.IChEMBLManager;
+import net.bioclipse.chembl.business.WizardHelper;
 import net.bioclipse.core.ResourcePathTransformer;
 import net.bioclipse.core.business.BioclipseException;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
 public class ChemblWizard extends Wizard implements INewWizard {
 
-    private static final Logger logger = Logger.getLogger(ChemblWizard.class);
-    private ChemblWizardPage firstpage;
-    private ChemblWizardPage2 secondpage;
-    private IChEMBLManager chembl;
-    ChemblData data = new ChemblData();
- 
-    public ChemblWizard() {
-        super();
-        setWindowTitle("ChEMBL wizard");
-        chembl = Activator.getDefault().getJavaChEMBLManager();
-    }
+	private static final Logger logger = Logger.getLogger(ChemblWizard.class);
+	private ChemblWizardPage firstpage;
+//	private ChemblWizardPage2 secondpage;
+	private WizardNewFileCreationPage filePage;
+	private IChEMBLManager chembl;
+	ChemblData data = new ChemblData();
 
-    public void addPages(){
-        firstpage = new ChemblWizardPage("ChEMBL page");
-        addPage(firstpage);
-        secondpage = new ChemblWizardPage2("ChEMBL page");
-        addPage(secondpage);
-    }    
-    @ Override
-    public boolean isHelpAvailable() {
-        return true;
-    }
-    //Writes compounds to a MoSS supported file
-    @Override
-    public boolean performFinish() {		
-    	try {
-			chembl.saveCSVT(data.file,data.tab, null);
+	public ChemblWizard() {
+		super();
+		setWindowTitle("ChEMBL wizard");
+		chembl = Activator.getDefault().getJavaChEMBLManager();
+	}
+
+	public void addPages(){
+		firstpage = new ChemblWizardPage("ChEMBL page");
+		addPage(firstpage);
+		//        secondpage = new ChemblWizardPage2("ChEMBL page");
+		//        addPage(secondpage);
+
+		ISelection sel=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+		if(sel instanceof IStructuredSelection){
+			filePage = new WizardNewFileCreationPage("newFilePage", (IStructuredSelection) sel);
+
+			filePage.setFileName( WizardHelper.findUnusedFileName((IStructuredSelection)sel, "chembl", ".txt") );
+		}else{
+			filePage = new WizardNewFileCreationPage("newFilePage", StructuredSelection.EMPTY);
+		}
+		filePage.setTitle("Choose location and add a file name to create a new file");
+		filePage.setDescription("Extension will be .txt if none is given");
+		addPage(filePage);
+	}	
+
+	@ Override
+	public boolean isHelpAvailable() {
+		return true;
+	}
+	//Writes compounds to a MoSS supported file
+	@Override
+	public boolean performFinish() {		
+		try {
+			if(filePage.getFileExtension()==null || filePage.getFileExtension().equals( "" ))
+				filePage.setFileExtension( "txt" );
+			IFile newFile= filePage.createNewFile();
+			newFile.delete( true, null );
+			chembl.saveCSVT(newFile,data.tab, null);
+//old version			chembl.saveCSVT(data.file,data.tab, null);
 		} catch (BioclipseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-    	return true;
-    }
-    
-//    public void saveCSV(String in, Table tab, IProgressMonitor monitor)
-//	throws BioclipseException, IOException {
-//		
-//		IFile filename = ResourcePathTransformer.getInstance().transform(in);
-//		if (filename.exists()) {
-//			throw new BioclipseException("File already exists!");
-//		}
-//		if (monitor == null)
-//			monitor = new NullProgressMonitor();
-//		monitor.beginTask("Writing file", 100);
-//		try {
-//			ByteArrayOutputStream output = new ByteArrayOutputStream();
-//			TableItem[] ti = tab.getItems();			
-//			for(int i =0; i< ti.length;i++){
-//				String s="";
-//				for(int j = 0; j < tab.getColumnCount(); j++){  
-//					if(ti[i].getText(j).equals("")){
-//						j = tab.getColumnCount();
-//					}else s= s+ ti[i].getText(j) + ",";
-//				}
-//				s=s+"\n";
-//				byte but[]= s.getBytes();
-//				output.write(but);	
-//			}
-//			output.close();
-//			filename.create(
-//					new ByteArrayInputStream(output.toByteArray()),
-//					false,
-//					monitor
-//			);
-//		}
-//		catch (Exception e) {
-//			monitor.worked(100);
-//			monitor.done();
-//			throw new BioclipseException("Error while writing file.", e);
-//		}
-//
-//		monitor.worked(100);
-//		monitor.done();
-//	}
- 
-
+		return true;
+	}
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
 
